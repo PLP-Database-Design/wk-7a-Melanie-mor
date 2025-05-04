64bc3ab2056147f8a6667c9ccddaf1d3aa7653a6
Melanie-mor <132539749+Melanie-mor@users.noreply.github.com>
2025-05-04 14:51:24 +0300

    Update answers.sql

--- answers.sql
+++ answers.sql
@@ -1,6 +1,13 @@
 -- QUESTION1
 -- 1NF
-CREATE TABLE PRODUCTDETAILEDNORMALISED (OrderID, CustomerName, Products)
+CREATE TABLE ProductDetails(
+ OrderID INT PRIMARY KEY,
+ CustomerName Varchar(100),
+ Products Varchar(100)
+ );
+
+INSERT INTO ProductDetails (OrderID, CustomerName, Products)
+ VALUES
 (101, Joe Doe, Laptop),
 (101, Joe Doe, Mouse),
 (102, Jane Smith, Tablet),
@@ -14,10 +21,27 @@ CREATE TABLE PRODUCTDETAILEDNORMALISED (OrderID, CustomerName, Products)
     OrderID INT PRIMARY KEY,
     CustomerName VARCHAR(100)
 );
-CREATE TABLE OrderDetails (
+INSERT INTO Orders (OrderID, CustomerName)
+ VALUES
+(101, Joe Doe),
+(101, Joe Doe),
+(102, Jane Smith),
+(102, Jane Smith),
+(102, Jane Smith),
+(103, Emily Clark);
+
+CREATE TABLE Products (
     OrderID INT,
     Product VARCHAR(100),
     Quantity INT,
     PRIMARY KEY (OrderID, Product),
     FOREIGN KEY (OrderID) REFERENCES Orders(OrderID)
 );
+INSERT INTO Products (OrderID, Products, Quantity)
+ VALUES
+(101, Laptop, 2),
+(101, Mouse, 1),
+(102, Tablet, 3),
+(102, Keyboard, 1),
+(102, Mouse, 2),
+(103, Phone, 1);
